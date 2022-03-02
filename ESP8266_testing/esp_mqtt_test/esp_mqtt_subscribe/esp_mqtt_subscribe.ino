1ea8eedde29d3d19ede048796c729d6753ae6a69
chrisswagler <chrisswagler22@gmail.com>
2022-03-01 20:36:58 -0500

    Update ESP subscriber and start pi changes

--- ESP8266_testing/esp_mqtt_test/esp_mqtt_subscribe/esp_mqtt_subscribe.ino
+++ ESP8266_testing/esp_mqtt_test/esp_mqtt_subscribe/esp_mqtt_subscribe.ino
@@ -6,6 +6,10 @@
 #include <ESP8266WiFi.h> // Enables the ESP8266 to connect to the local network (via WiFi)
 #include <PubSubClient.h> // Allows us to connect to, and publish to the MQTT broker
 
+// Helper macro to merge bytes
+#define TO_INT16(a,b) (((b)<<8)|(a))
+#define TO_INT32(a,b,c,d) (((d)<<24)|((c)<<16)|((b)<<8)|(a))
+
 const int ledPin = 0; // This code uses the built-in led for visual feedback that a message has been received
 
 // WiFi
@@ -17,34 +21,73 @@ const char* wifi_password = "Your WiFi Password";
 // Make sure to update this for your own MQTT Broker!
 const char* mqtt_server = "Your MQTT Server IP Address";
 const char* mqtt_topic = "Your MQTT Topic";
-const char* mqtt_username = "Your MQTT Username";
-const char* mqtt_password = "Your MQTT Password";
-// The client id identifies the ESP8266 device. Think of it a bit like a hostname (Or just a name, like Greg).
+// The client id identifies the ESP8266 device. 
 const char* clientID = "ESP8266_1";
 
 // Initialise the WiFi and MQTT Client objects
 WiFiClient wifiClient;
 PubSubClient client(mqtt_server, 1883, wifiClient); // 1883 is the listener port for the Broker
 
-void ReceivedMessage(char* topic, byte* payload, unsigned int length) {
-  // Output the first character of the message to serial (debug)
-  Serial.println((char)payload[0]);
-
-  // Handle the message we received
-  // Here, we are only looking at the first character of the received message (payload[0])
-  // If it is 0, turn the led off.
-  // If it is 1, turn the led on.
-  if ((char)payload[0] == '0') {
-    digitalWrite(ledPin, HIGH); // Notice for the HUZZAH Pin 0, HIGH is OFF and LOW is ON. Normally it is the other way around.
+// Given an array of bytes, converts to integers and stores in converted[]
+// Assumes byteArry is of size 32 and that two adjacent bytes represent an int
+void readInt(byte* byteArray, int converted[], unsigned int length)
+{
+  int byteNum = 0;
+  byte result[2];
+  for (int i = 0; i < (length / 2); i++) {
+    for (int j = 0; j < 2; j++) {
+      result[j] = byteArray[byteNum];
+      byteNum++;
+    }
+    converted[i] = TO_INT16(result[0], result[1]);
   }
-  if ((char)payload[0] == '1') {
+}
+
+// Given two integer arrays and ints representing their lengths,
+// determines if the arrays are equal (in length and sequentially)
+bool intArraysEqual(int array1[], int length1, int array2[], int length2)
+{
+  //  if (sizeof(array1) != sizeof(array2)) {
+  if (length1 != length2) {
+    return false;
+  }
+  for (int i = 0; i < length1; i++) {
+    if (array1[i] != array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void ReceivedMessage(char* topic, byte* payload, unsigned int length) {
+  // Convert the payload to an integer array
+  int convertedMessage[length / 2];
+  readInt(payload, convertedMessage, length);
+
+  int expectedMessage[] = {1, 2, 3, 4, 5, 6};
+  int expectedSize = 6;
+  // Handle the message we received
+  if (intArraysEqual(convertedMessage, length / 2, expectedMessage, expectedSize)) {
+    digitalWrite(ledPin, HIGH);
+  } else {
     digitalWrite(ledPin, LOW);
   }
+  
+//  // Here, we are only looking at the first character of the received message (payload[0])
+//  // If it is 0, turn the led off.
+//  // If it is 1, turn the led on.
+//  if ((char)payload[0] == '0') {
+//    digitalWrite(ledPin, HIGH); // Notice for the HUZZAH Pin 0, HIGH is OFF and LOW is ON. Normally it is the other way around.
+//  }
+//  if ((char)payload[0] == '1') {
+//    digitalWrite(ledPin, LOW);
+//  }
 }
 
 bool Connect() {
   // Connect to MQTT Server and subscribe to the topic
-  if (client.connect(clientID, mqtt_username, mqtt_password)) {
+//  if (client.connect(clientID, mqtt_username, mqtt_password)) {
+    if (client.connect(clientID)) {
       client.subscribe(mqtt_topic);
       return true;
     }
--- ESP8266_testing/pi/pi_mqtt_test.py
+++ ESP8266_testing/pi/pi_mqtt_test.py
@@ -15,7 +15,7 @@ client = mqtt.Client()
 # to the broker, and what happens then the topic receives a message
 def on_connect(client, userdata, flags, rc):
     # rc is the error code returned when connecting to the broker
-    print "Connected!", str(rc)
+    print("Connected!" + str(rc))
     
     # Once the client has connected to the broker, subscribe to the topic
     client.subscribe(mqtt_topic)
@@ -25,15 +25,30 @@ def on_message(client, userdata, msg):
     # If you want to check each message, and do something depending on
     # the content, the code to do this should be run in this function
     
-    print "Topic: ", msg.topic + "\nMessage: " + str(msg.payload)
+    print("Topic: " + msg.topic + "\nMessage: " + str(msg.payload))
     
     # The message itself is stored in the msg variable
     # and details about who sent it are stored in userdata
 
+def on_publish(client, userdata, mid):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
 # Here, we are telling the client which functions are to be run
-# on connecting, and on receiving a message
+# on connecting, on receiving a message, and on publishing
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_publish = on_publish
 
 # Once everything has been set up, we can (finally) connect to the broker
 # 1883 is the listener port that the MQTT broker is using
